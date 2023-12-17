afee6b72c36016b641bd9bb8fcd8878caabad3f0
Felipe Barreto Pereira <fbarreto.dev@gmail.com>
2023-12-17 16:18:48 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -5,18 +5,30 @@ interface SutTypes {
   sut: BcryptAdapter
 }
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise((resolve) => { resolve('hash') })
+  }
+}))
+
 describe('BCrypt Adapter', () => {
+  const salt = 12
+
   const makeSut = (): SutTypes => {
-    const sut = new BcryptAdapter(12)
+    const sut = new BcryptAdapter(salt)
     return {
       sut
     }
   }
   test('Should call bcrypt with correct values', async () => {
-    const salt = 12
     const { sut } = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+  test('Should return a hash on success', async () => {
+    const { sut } = makeSut()
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hash')
   })
 })
