20061a7a7b068747668ecce693ed708270364350
Felipe Barreto Pereira <fbarreto.dev@gmail.com>
2023-12-17 22:58:56 -0300

    feat: ensure AccountMongoRepository returns account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,20 @@
+import { type AddAccountRepository } from '@/data/protocols/add-account-repository'
+import { type AccountModel } from '@/domain/models/account'
+import { type AddAccountModel } from '@/domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const createdAccountId = result.insertedId
+    const createdAccount = await accountCollection.findOne({ _id: createdAccountId })
+    const account = {
+      id: createdAccount._id.toString(),
+      name: createdAccount.name,
+      email: createdAccount.email,
+      password: createdAccount.password
+    }
+    return account
+  }
+}
