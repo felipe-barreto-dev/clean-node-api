32e5fdc9b9ee4122fa75c93c109310ed07ff9e2e
Felipe Barreto Pereira <fbarreto.dev@gmail.com>
2023-12-17 23:13:35 -0300

    refactor: remove all accounts before each test

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -9,6 +9,10 @@ describe('Account Mongo Repository', () => {
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
   test('Should return an account on success', async () => {
     const sut = new AccountMongoRepository()
     const account = await sut.add({
