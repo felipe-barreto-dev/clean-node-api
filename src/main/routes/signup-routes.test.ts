eb410395daff9ab1fcb87fe2918f6f760326363a
Felipe Barreto Pereira <fbarreto.dev@gmail.com>
2023-12-18 20:55:24 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,16 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
