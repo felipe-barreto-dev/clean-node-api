ec906204ca33e749a957cf4cc04e720cb2d1e96b
Felipe Barreto Pereira <fbarreto.dev@gmail.com>
2023-12-18 21:18:50 -0300

    feat: add SignUp Factory

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,13 +1,10 @@
-import { type ConnectOptions, MongoClient, type Collection } from 'mongodb'
+import { MongoClient, type Collection } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
   async connect (uri: string): Promise<void> {
     // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-    this.client = await MongoClient.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    } as ConnectOptions)
+    this.client = await MongoClient.connect(uri)
   },
   async disconnect () {
     await this.client.close()
