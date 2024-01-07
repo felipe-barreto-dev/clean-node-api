09b358ca4ba0b3fb01cebe96ec77f07e835531e0
Felipe Barreto Pereira <fbarreto.dev@gmail.com>
2024-01-06 22:32:40 -0300

    feat: add login route

---
+++ src/main/routes/login-routes.test.ts
@@ -0,0 +1,54 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { type Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
+
+describe('Login Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Felipe',
+          email: 'felipe@teste.com',
+          password: 'Teste123!',
+          passwordConfirmation: 'Teste123!'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('Teste123!', 12)
+      await accountCollection.insertOne({
+        name: 'Felipe',
+        email: 'felipe@teste.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'felipe@teste.com',
+          password: 'Teste123!'
+        })
+        .expect(200)
+    })
+  })
+})
