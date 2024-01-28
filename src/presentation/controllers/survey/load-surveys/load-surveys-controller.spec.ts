2e0a041db9cf32b17913e7fa4dbd438c333e8b3a
Felipe Barreto Pereira <fbarreto.dev@gmail.com>
2024-01-28 12:42:06 -0300

    test: ensure loadSurveysController is working

---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -0,0 +1,67 @@
+import { type SurveyModel, serverError, type LoadSurveys, ok } from './load-surveys-protocols'
+import { LoadSurveysController } from './load-surveys-controller'
+
+interface SutTypes {
+  sut: LoadSurveysController
+  loadSurveysStub: LoadSurveys
+}
+
+const makeFakeSurveys = [{
+  question: 'Question 1',
+  answers: [{
+    answer: 'Answer 1'
+  },
+  {
+    answer: 'Answer 2'
+  }]
+},
+{
+  question: 'Question 2',
+  answers: [{
+    answer: 'Answer 1'
+  },
+  {
+    answer: 'Answer 2'
+  }]
+}]
+
+const makeLoadSurveys = (): LoadSurveys => {
+  class LoadSurveysStub implements LoadSurveys {
+    async load (): Promise<SurveyModel[]> {
+      return makeFakeSurveys
+    }
+  }
+  return new LoadSurveysStub()
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveysStub = makeLoadSurveys()
+  const sut = new LoadSurveysController(loadSurveysStub)
+
+  return {
+    sut,
+    loadSurveysStub
+  }
+}
+
+describe('LoadSurveys Controller', () => {
+  test('Should call LoadSurveys', async () => {
+    const { sut, loadSurveysStub } = makeSut()
+    const loadSpy = jest.spyOn(loadSurveysStub, 'load')
+    await sut.handle()
+    expect(loadSpy).toHaveBeenCalled()
+  })
+
+  test('Should return 500 if LoadSurveys returns an error', async () => {
+    const { sut, loadSurveysStub } = makeSut()
+    jest.spyOn(loadSurveysStub, 'load').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+    const httpResponse = await sut.handle()
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle()
+    expect(httpResponse).toEqual(ok(makeFakeSurveys))
+  })
+})
