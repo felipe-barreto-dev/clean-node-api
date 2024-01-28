06870f903aec39bc96b27596dbcee8deef25d747
Felipe Barreto Pereira <fbarreto.dev@gmail.com>
2024-01-28 13:13:16 -0300

    test: ensure DbLoadSurveys is working

---
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -0,0 +1,65 @@
+import { DbLoadSurveys } from './db-load-surveys'
+import { type LoadSurveysRepository, type SurveyModel } from './db-load-surveys-protocols'
+
+describe('DbLoadSurveys Usecase', () => {
+  const makeFakeSurveys: SurveyModel[] = [{
+    question: 'Question 1',
+    answers: [{
+      answer: 'Answer 1'
+    },
+    {
+      answer: 'Answer 2'
+    }]
+  },
+  {
+    question: 'Question 2',
+    answers: [{
+      answer: 'Answer 1'
+    },
+    {
+      answer: 'Answer 2'
+    }]
+  }]
+
+  const makeLoadSurveysRepository = (): LoadSurveysRepository => {
+    class LoadSurveysRepositoryStub implements LoadSurveysRepository {
+      async load (): Promise<SurveyModel[]> {
+        return makeFakeSurveys
+      }
+    }
+    return new LoadSurveysRepositoryStub()
+  }
+
+  interface SutTypes {
+    sut: DbLoadSurveys
+    loadSurveysRepositoryStub: LoadSurveysRepository
+  }
+  const makeSut = (): SutTypes => {
+    const loadSurveysRepositoryStub = makeLoadSurveysRepository()
+    const sut = new DbLoadSurveys(loadSurveysRepositoryStub)
+    return {
+      sut,
+      loadSurveysRepositoryStub
+    }
+  }
+
+  test('Should call LoadSurveysRepository', async () => {
+    const { sut, loadSurveysRepositoryStub } = makeSut()
+    const loadSurveysRepositorySpy = jest.spyOn(loadSurveysRepositoryStub, 'load')
+    await sut.load()
+    expect(loadSurveysRepositorySpy).toHaveBeenCalled()
+  })
+
+  test('Should throw if LoadSurveysRepository throws', async () => {
+    const { sut, loadSurveysRepositoryStub } = makeSut()
+    jest.spyOn(loadSurveysRepositoryStub, 'load').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return surveys on success', async () => {
+    const { sut } = makeSut()
+    const surveys = await sut.load()
+    expect(surveys).toEqual(makeFakeSurveys)
+  })
+})
