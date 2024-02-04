3598bd91d10686996503cac62a68eb399c1eb59a
Felipe Barreto Pereira <fbarreto.dev@gmail.com>
2024-02-04 07:54:29 -0300

    feat: add survey result routes

---
+++ src/main/routes/survey-result-routes.ts
@@ -0,0 +1,10 @@
+import { type Router } from 'express'
+import { adaptMiddleware } from '../adapters/express/express-middleware-adapter'
+import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
+import { adaptRoute } from '../adapters/express/express-route-adapter'
+import { makeSaveSurveyResultController } from '../factories/controllers/save-survey-result/save-survey-result-controller-factory'
+
+export default (router: Router): void => {
+  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
+  router.put('/surveys/:surveyId/results', adminAuth, adaptRoute(makeSaveSurveyResultController()))
+}
