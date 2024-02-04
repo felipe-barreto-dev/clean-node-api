e7666eb4591d915bc879e1698fbb9a4cdaf3dca9
Felipe Barreto Pereira <fbarreto.dev@gmail.com>
2024-02-04 07:09:35 -0300

    feat: add LoadSurveysController route

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -3,8 +3,10 @@ import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeAddSurveyController } from '../factories/controllers/add-survey/add-survey-controller-factory'
 import { adaptMiddleware } from '../adapters/express/express-middleware-adapter'
 import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
+import { makeLoadSurveysController } from '../factories/controllers/load-surveys/load-surveys-controller-factory'
 
 export default (router: Router): void => {
   const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
+  router.get('/surveys', adminAuth, adaptRoute(makeLoadSurveysController()))
 }
