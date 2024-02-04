c6b153e8f0fb47450519808c021b0b7fc2e11d1a
Felipe Barreto Pereira <fbarreto.dev@gmail.com>
2024-02-04 16:40:00 -0300

    feat: add LoadSurveyResultController

---
+++ src/presentation/controllers/survey/load-survey-result/load-survey-result-controller.ts
@@ -0,0 +1,32 @@
+import { type HttpRequest, type Controller, type HttpResponse } from '@/presentation/protocols'
+import { forbidden, serverError, ok } from '@/presentation/helpers'
+import { InvalidParamError } from '@/presentation/errors'
+import { type CheckSurveyById, type LoadSurveyResult } from '@/domain/usecases'
+
+export class LoadSurveyResultController implements Controller {
+  constructor (
+    private readonly checkSurveyById: CheckSurveyById,
+    private readonly loadSurveyResult: LoadSurveyResult
+  ) {}
+
+  async handle (request: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { surveyId, accountId } = request.body
+      const exists = await this.checkSurveyById.checkById(surveyId)
+      if (!exists) {
+        return forbidden(new InvalidParamError('surveyId'))
+      }
+      const surveyResult = await this.loadSurveyResult.load(surveyId, accountId)
+      return ok(surveyResult)
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
+
+export namespace LoadSurveyResultController {
+  export type Request = {
+    surveyId: string
+    accountId: string
+  }
+}
