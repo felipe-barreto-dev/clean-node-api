ba3495b9e67fd977be0a39e7f4ead86871940550
Felipe Barreto Pereira <fbarreto.dev@gmail.com>
2025-10-11 18:37:33 -0300

    feat: export strong password validation module

--- src/validation/validators/index.ts
+++ src/validation/validators/index.ts
@@ -1,4 +1,5 @@
 export * from './compare-fields-validation'
 export * from './email-validation'
 export * from './required-field-validation'
+export * from './strong-password-validation'
 export * from './validation-composite'
